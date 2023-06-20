2b67b1e4913ade8a4a4510c0ecd40f682016f33f
shambu <rajeshsabareenadh@gmail.com>
2023-06-20 21:12:43 +0530

    calci

--- firstcod.py
+++ firstcod.py
@@ -1,2 +1,16 @@
-print("hello\"world\"",37583,sep="|",end="\n")
-print("cjjcw")
+a=float(input("enter two numbers"))
+b=float(input())
+print("press the following number asssigned to the arithematic progression\n1.add\n2.sub\n3.multiply\n4.divide")
+x=int(input())
+def calc(x):
+ if x==1:
+  print(a+b)
+ elif x==2:
+    print(a-b)
+ elif x==3:
+     print(a*b)
+ elif x==4:
+     print(a/b)
+ else:
+    print("invalid")
+calc(x)
