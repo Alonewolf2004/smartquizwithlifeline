2aabf7bd2438e0d13590917b7323c539811fcf81
shambu <rajeshsabareenadh@gmail.com>
2023-06-21 17:11:09 +0530

    numberdetector

--- firstcod.py
+++ firstcod.py
@@ -1,16 +1,6 @@
-a=float(input("enter two numbers"))
-b=float(input())
-print("press the following number asssigned to the arithematic progression\n1.add\n2.sub\n3.multiply\n4.divide")
-x=int(input())
-def calc(x):
- if x==1:
-  print(a+b)
- elif x==2:
-    print(a-b)
- elif x==3:
-     print(a*b)
- elif x==4:
-     print(a/b)
- else:
-    print("invalid")
-calc(x)
+a = input("enter the numbers")
+b= len(a)
+if a.isdigit() and b == 10:
+    print("num is valid")
+else:
+    print("num is not valid try again")
