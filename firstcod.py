6d09a1268514f69fcc4916c1580515d0a3076b89
shambu <rajeshsabareenadh@gmail.com>
2023-06-26 20:56:29 +0530

    wrote prime code

--- firstcod.py
+++ firstcod.py
@@ -1,10 +1,10 @@
 import time
-a=time.strftime("%H:%M:%S")
-if(a>="24:00:00" and a<="11:59:59"):
+a=time.strftime("%H")
+if(a>="24" and a<="11"):
     print("GOOD MORNING,SIR.")
-elif(a>="12:00:00" and a<="17:59:59"):
+elif(a>="12" and a<="17"):
     print("GOOD AFTERNOON,SIR.")  
-elif(a>="18:00:00" and a<="20:59:59"):
+elif(a>="18" and a<="20"):
     print("GOOD EVENING,SIR.") 
 else:
     print("GOOD NIGHT,SIR.")      
