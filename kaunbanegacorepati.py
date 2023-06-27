d485367de3656c68fbe6c39f360a7732ee6e0dfa
shambu <rajeshsabareenadh@gmail.com>
2023-06-27 21:23:18 +0530

    kaunbanegacororepati

---
+++ kaunbanegacorepati.py
@@ -0,0 +1,9 @@
+ONE=["What is the chemical symbol for hydrogen?","a.H","b.c","c.NO","d.o"]
+print(ONE[0],"\n",ONE[1],"               ",ONE[2],"\n",ONE[3],"              ",ONE[4])
+print("enter your answer")
+A=input()
+if (A=="a"):
+    print("your are correct")
+else:
+    print("you are incorrect")
+
