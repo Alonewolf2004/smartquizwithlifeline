15d5c5634053c7170380bddf6743e089b30a7524
shambu <rajeshsabareenadh@gmail.com>
2023-06-29 00:03:21 +0530

    smartquiz with lifeline

--- smartquiz.py
+++ smartquiz.py
@@ -1,27 +1,51 @@
-questions=[["What is the chemical symbol for hydrogen?","a.H","b.c","c.NO","d.o","1"],
-           ["What is the smallest unit of matter?","a) cell","b) Molecule","c) atom","d) Nucleus","3"],
-           ["What is the smallest unit of matter?","a) cell","b) Molecule","c) atom","d) Nucleus","3"],
-           ["What is the smallest unit of matter?","a) cell","b) Molecule","c) atom","d) Nucleus","3"]]
-prize=[1000,2000,3000,5000]
-for i in range(len(questions)):
- print(questions[i][0])
- print(questions[i][1],"          ",questions[i][2])
- print(questions[i][3],"          ",questions[i][4])
- a=input("\nif you want yo use a lifeline press 3 :")
- if(a=="3"):
-  x=questions[i][-1]
-  y=len(questions[i])
-  for j in range(1,5):
-   if(x!=j):  
-    questions[i].pop(j)
-    print(questions[i][0])
-    print(questions[i][1],"          ",questions[i][2])
-    print(questions[i][3],"          ",questions[i][4])
+def display_questions(question,options):
+    print(question)
+    for i,option in enumerate(options,start=1):
+        print(f"{i}. {option}")
 
- if(a==questions[i][-1]):
-  print("you are correct")
-  continue
- else:
-  print("you are incorrect")  
-
-  break
+def fifty_fifty(options,correctoptions):
+   incorrectoption=[opt for i,opt in enumerate(options,start=1) if i != correctoptions] 
+   incorrectoption=incorrectoption[:len(incorrectoption)//2]
+   updated_opt=[]
+   for i,option in enumerate(options,start=1):
+      if(i==correctoptions or option in incorrectoption):
+         updated_opt.append(option)
+      else:
+         updated_opt.append("    ")
+   return updated_opt
+prize=[1000,2000,3000,5000,10000,20000,40000,80000]
+def print_quiz(questions):
+  
+   score=0
+   lifeline=1
+   print("welcome to the quiz\n press 1 to start the quiz")
+   x=int(input())
+   for i,(question,options,correctoptions) in enumerate(questions,start=1):
+      if ( x==1):
+         display_questions(question,options)
+         print("enter your answer by pressing (1:4) or type 9 to use 50-50 option")
+         y=(input())
+         if y==str(correctoptions) :
+            print("you are correct")
+            score+=1
+         elif y == "9" and lifeline == 1:
+                updated_opt= fifty_fifty(options, correctoptions)
+                lifeline = 0
+                display_questions(question, updated_opt)
+                print("Enter your answer by pressing (1-4).")
+                y = input()
+                if y == str(correctoptions):
+                    print("You are correct.")
+                    score += 1
+         else:
+            print("you are incorrect")
+   for i in range(8): 
+      if i== score :
+         print(prize[i-1])      
+questions = [
+    ("What is the capital of France?", ["London", "Paris", "Berlin", "Madrid"], 2),
+    ("Which planet is known as the Red Planet?", ["Mars", "Jupiter", "Venus", "Saturn"], 1),
+    ("Who painted the Mona Lisa?", ["Leonardo da Vinci", "Vincent van Gogh", "Pablo Picasso", "Michelangelo"], 1),
+    ("What is the largest ocean in the world?", ["Pacific Ocean", "Indian Ocean", "Atlantic Ocean", "Arctic Ocean"], 1)
+]
+print_quiz(questions)
