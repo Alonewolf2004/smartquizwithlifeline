205c949fb0ded41b5b5603eb2466221df1bf686c
shambu <rajeshsabareenadh@gmail.com>
2023-07-09 22:46:48 +0530

    rps

--- onlinecode.py
+++ onlinecode.py
@@ -1,24 +1,27 @@
 import random
+round=int(input("how many rounds you want to play ?"))
+score=0
+for i in range(round):
+   user_input=input(" press 'r' for rock,'p' for paper,'s' for scissor:")
+   elements=["r","p","s"]
+   comp=random.choice(elements)
+   print(comp)
+   
+   if user_input==comp:   
+    print("draw")
+   elif user_input=="r" and comp=="p" or comp=="s":   
+       print("comp won ")
 
-def checking():
-    score = 0
-    comp = ["r", "p", "s"]
-    random.shuffle(comp)
-    rounds = int(input("How many rounds do you want to play? "))
-    for _ in range(rounds):
-        user_input = input("Type 'r' for rock, 'p' for paper, or 's' for scissors: ")
-        if user_input == "r" or user_input == "p" or user_input == "s":
-            comp_choice = random.choice(comp)
-            print("Computer's choice:", comp_choice)
-            if user_input == comp_choice:
-                score += 1
-        else:
-            print("Invalid input. You have to enter 'r', 'p', or 's'.")
-    return score
+   elif user_input=="p" and comp=="r" or comp=="s":
+       print("comp won ")
 
-user_consent = input("Press 'y' to play or 'n' to not play: ")
-if user_consent == "y":
-    score = checking()
-    print("Final score:", score)
+   elif user_input=="s" and comp=="p" or comp=="r":
+    print("comp won ")
+    
+   else:
+       print("you won")
+       score+=1        
+if score>=round:              
+ print("you won with a score of  score",score)       
 else:
-    print("Thank you. Have a nice day.")
+    print("you lost with a score of  score",score)       
--- rockpaper.py
+++ rockpaper.py
@@ -3,10 +3,13 @@ round=int(input("how many rounds you want to play ?"))
 score=0
 for i in range(round):
    user_input=input(" press 'r' for rock,'p' for paper,'s' for scissor:")
-   elements=["r","p","s"]
-   comp=random.choice(elements)
-   print(comp)
-   
+   if user_input =="r" or user_input =="p" or user_input =="s" :
+
+    elements=["r","p","s"]
+    comp=random.choice(elements)
+   else: 
+    print("invalid input")
+    break
    if user_input==comp:   
     print("draw")
    elif user_input=="r" and comp=="p" or comp=="s":   
@@ -25,3 +28,4 @@ if score>=round:
  print("you won with a score of  score",score)       
 else:
     print("you lost with a score of  score",score)       
+   
