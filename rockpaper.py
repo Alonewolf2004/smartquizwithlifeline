7f625254b4da5ffed42a5d6a0258d3375d8d3227
shambu <rajeshsabareenadh@gmail.com>
2023-07-09 21:14:19 +0530

    rock papaer 2

--- rockpaper.py
+++ rockpaper.py
@@ -1,24 +1,27 @@
 import random
-def checking():
-    score=0
-    comp=["r","p","s"]
-    random.shuffle(comp)
-    rounds=int(input(" how many rounds do you want to play ?"))
-    for i in range(rounds): 
-       for i in range(0,3):
-           user_input=input("type for 'r' for rock ,  'p' for paper , 's' for scissor :")
-           if user_input== "r"or user_input== "p" or user_input== "s":
-             if user_input==comp[i]:
-                score+=1
-                continue
-           else:
-             print("invalid input you have to put r,p or s") 
-         
-    return score           
-user_consent=input("press 'y' to play 'n' to not play :")
-if user_consent=="y":
-  score=checking()
-  print(score) 
+round=int(input("how many rounds you want to play ?"))
+score=0
+for i in range(round):
+   user_input=input(" press 'r' for rock,'p' for paper,'s' for scissor:")
+   elements=["r","p","s"]
+   comp=random.choice(elements)
+   print(comp)
+   
+   if user_input==comp:   
+    print("draw")
+   elif user_input=="r" and comp=="p" or comp=="s":   
+       print("comp won ")
+
+   elif user_input=="p" and comp=="r" or comp=="s":
+       print("comp won ")
+
+   elif user_input=="s" and comp=="p" or comp=="r":
+    print("comp won ")
+    
+   else:
+       print("you won")
+       score+=1        
+if score>=round:              
+ print("you won with a score of  score",score)       
 else:
-   print("thank you have a nice day. ")
- 
+    print("you lost with a score of  score",score)       
